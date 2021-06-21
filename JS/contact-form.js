26e85c73d3044b89c9fab103a3c279f445f9d1df
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-21 22:00:25 +0100

    Slideshow responsive sizes

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -6,6 +6,8 @@ function closeContact() {
   document.getElementById("myContact").style.display = "none";
 }
 
+var modal = document.getElementById('myContact');
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
