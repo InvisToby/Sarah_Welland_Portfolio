64e33c386fabbc9561d646b66b473780a87075de
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-22 20:30:10 +0100

    Sticky Navbar

---
+++ JS/stickNav.js
@@ -0,0 +1,25 @@
+var height = $('.bannerHere').height();
+
+$(window).scroll(function () {
+  if($(this).scrollTop()>height){ 
+    $('.navbar').addClass('sticky');
+  }else{
+    $('.navbar').removeClass('sticky');
+  }
+});
+
+//alt test
+//var navbar = document.getElementsByClassName("navbar");
+
+//var sticky = navbar.offsetTop
+
+//window.onscroll = function () {
+//    if (window.scrollY > sticky) { 
+//       navbar.classList.add("sticky");
+//    }else{
+//        navbar.classList.remove("sticky");
+//    }
+//  };
+
+
+  
