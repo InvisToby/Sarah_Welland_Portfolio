cc011592af0d6cfcb79ba5db9a9dcf2510dc245a
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-22 20:42:24 +0100

    Sticky nav link test and alt js test

--- JS/stickNav.js
+++ JS/stickNav.js
@@ -1,25 +1,23 @@
-var height = $('.bannerHere').height();
+//alt test haven't got to work...
 
-$(window).scroll(function () {
-  if($(this).scrollTop()>height){ 
-    $('.navbar').addClass('sticky');
-  }else{
-    $('.navbar').removeClass('sticky');
-  }
-});
-
-//alt test
 //var navbar = document.getElementsByClassName("navbar");
-
 //var sticky = navbar.offsetTop
 
 //window.onscroll = function () {
-//    if (window.scrollY > sticky) { 
-//       navbar.classList.add("sticky");
+//    if (window.scrollY > sticky) {
+//      navbar.classList.add("sticky");
 //    }else{
 //        navbar.classList.remove("sticky");
-//    }
-//  };
+//    }};
 
+  
+  var height = $('.bannerHere').height();
 
+  $(window).scroll(function () {
+    if($(this).scrollTop()>height){ 
+      $('.navbar').addClass('sticky');
+    }else{
+      $('.navbar').removeClass('sticky');
+    }
+  });
   
