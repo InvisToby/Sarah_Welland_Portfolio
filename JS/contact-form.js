4f0c3ba566031b685f2e4285a0204184d7a3f3dc
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-23 17:11:06 +0100

    errors with slideshow, contact form closing now

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -6,11 +6,14 @@ function closeContact() {
   document.getElementById("myContact").style.display = "none";
 }
 
-var modal = document.getElementById('myContact');
 
+var modal = document.getElementById('myContact');
+var Bod = document.getElementsByTagName('body')[0];
+var Banner = document.getElementsByClassName('bannerHere')[0];
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  console.log(event);
+  if (event.target == Bod || event.target == Banner) {
     modal.style.display = "none";
   }
 }
