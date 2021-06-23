af80a4706389478a5948594321b8ce88d98422ce
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-23 23:25:13 +0100

    changing sizes, adding text

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -8,13 +8,17 @@ function closeContact() {
 
 
 var modal = document.getElementById('myContact');
+
+var title = document.getElementsById('title');
+var homes = document.getElementById('home');
 var Bod = document.getElementsByTagName('body')[0];
 var Banner = document.getElementsByClassName('bannerHere')[0];
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   console.log(event);
-  if (event.target == Bod || event.target == Banner) {
+  if (event.target == Bod || event.target == Banner || event.target == homes || event.target == title) {
     modal.style.display = "none";
   }
 }
-
