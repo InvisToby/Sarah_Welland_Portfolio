231136b92daca7a9873c6e9adecb4b4822176440
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-06-24 22:17:00 +0100

    sorting out closing of contact form

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -9,7 +9,8 @@ function closeContact() {
 
 var modal = document.getElementById('myContact');
 
-var title = document.getElementsById('title');
+var gall = document.getElementById('gallery');
+var title = document.getElementsByClassName('title')[0];
 var homes = document.getElementById('home');
 var Bod = document.getElementsByTagName('body')[0];
 var Banner = document.getElementsByClassName('bannerHere')[0];
@@ -18,7 +19,7 @@ var Banner = document.getElementsByClassName('bannerHere')[0];
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   console.log(event);
-  if (event.target == Bod || event.target == Banner || event.target == homes || event.target == title) {
+  if (event.target == Bod || event.target == Banner || event.target == homes || event.target == title || event.target == gall){
     modal.style.display = "none";
   }
 }
