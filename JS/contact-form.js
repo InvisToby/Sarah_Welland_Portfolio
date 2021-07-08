7158ddc5e068ade3a354be06039ae64be705a7d2
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-07-08 19:37:22 +0100

    scroll down animation, considering circles

--- JS/contact-form.js
+++ JS/contact-form.js
@@ -10,16 +10,18 @@ function closeContact() {
 var modal = document.getElementById('myContact');
 
 var gall = document.getElementById('gallery');
+var gallCom = document.getElementsByClassName('gallery-container')[0];
 var title = document.getElementsByClassName('title')[0];
 var homes = document.getElementById('home');
 var Bod = document.getElementsByTagName('body')[0];
 var Banner = document.getElementsByClassName('bannerHere')[0];
+var divy = document.getElementsByTagName('*')[0];
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   console.log(event);
-  if (event.target == Bod || event.target == Banner || event.target == homes || event.target == title || event.target == gall){
+  if (event.target == divy || event.target == gallCom || event.target == Bod || event.target == Banner || event.target == homes || event.target == title || event.target == gall){
     modal.style.display = "none";
   }
 }
